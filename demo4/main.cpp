c9b236eafebeec57134eacb88a37a8ee1871e912
Andrew <root.drew27@gmail.com>
2025-03-10 07:44:25 -0500

    Added types to sizes_demo

--- demo4/main.cpp
+++ demo4/main.cpp
@@ -7,14 +7,19 @@
 
 using namespace std;
 
+// 1
 void sizes_demo()
 {
     cout << "Size of int8_t: " << sizeof(int8_t) << endl;
     cout << "Size of int16_t: " << sizeof(int16_t) << endl;
     cout << "Size of int32_t: " << sizeof(int32_t) << endl;
     cout << "Size of int64_t: " << sizeof(int64_t) << endl;
+    cout << "Size of wchar_t: " << sizeof(wchar_t) << endl;
+    cout << "Size of char16_t: " << sizeof(char16_t) << endl;
+    cout << "Size of char: " << sizeof(char) << endl;
 }
 
+// 2
 void bitmap_demo()
 {
     unsigned long long every_other = 0xAAAA'AAAA'AAAA'AAAA;
@@ -32,12 +37,18 @@ void bitmap_demo()
     cout << "Bitmap shifted with pos 1 set: " << bs_every_other << endl;
 }
 
+// 3
 void endian_demo()
 {
+    cout << hex;
     unsigned short num = 0xff00;
-    cout << hex << "Before Flip: " << num << endl;
-    unsigned short converted_num = ((num & 0xff) << 8) | ((num & 0xff00) >> 8);
-    cout << "Endian Flip: " << converted_num << endl;
+    unsigned short num_flipped = ((num & 0x00ff) << 8) | ((num & 0xff00) >> 8);
+    cout << "Unsigned Short: " << num << endl;
+    cout << "Endian Flipped: " << num_flipped << endl;
+    cout << endl;
+    unsigned int num2 = 0xffff0000;
+    unsigned int num2_flipped = ((num2 & 0x0000ffff) << 16) | ((num2 & 0xffff0000) >> 16);
+    cout << "Unsigned Int: " << num2 << endl << "Endian Flipped: " << num2_flipped << endl << dec;
 }
 
 int main(int argc, char *argv[])
@@ -54,8 +65,7 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    string args = argv[1]; 
-    
+    string args = argv[1];
     for (int i = 0; i < args.length(); ++i)
     {
         switch (atoi(&args[i]))
